0eb998b3ce7e699b01494a728b7b906c60e2e4c5
innat <innat.dev@gmail.com>
2023-08-11 21:23:53 +0600

    update: pkg directory at src

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 def get_install_requirements():
-    with open("src/medicai/requirements.txt", "r", encoding="utf-8") as f:
+    with open("requirements.txt", "r", encoding="utf-8") as f:
         reqs = [x.strip() for x in f.read().splitlines()]
     reqs = [x for x in reqs if not x.startswith("#")]
     return reqs
