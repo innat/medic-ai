82fce9e93d8083b8524cd29c0e43d33f390aba23
innat <innat.dev@gmail.com>
2023-08-11 21:21:40 +0600

    update: pkg directory at src

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 
 def get_install_requirements():
-    with open("requirements.txt", "r", encoding="utf-8") as f:
+    with open("src/requirements.txt", "r", encoding="utf-8") as f:
         reqs = [x.strip() for x in f.read().splitlines()]
     reqs = [x for x in reqs if not x.startswith("#")]
     return reqs
 
 
 def get_eyenet_version():
-    with open("eyenet/__init__.py", "r") as f:
+    with open("src/medicai/__init__.py", "r") as f:
         version = re.search(
             r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
         ).group(1)
@@ -18,7 +18,7 @@ def get_eyenet_version():
 
 
 def get_long_description():
-    with open("README.md", "r", encoding="utf-8") as f:
+    with open("src/README.md", "r", encoding="utf-8") as f:
         long_description = f.read()
     return long_description
 
@@ -28,7 +28,7 @@ setup(
     version=get_eyenet_version(),
     author="innat",
     author_email="innat.dev@gmail.com",
-    url="https://github.com/innat/eye-net",
+    url="https://github.com/innat/medic-ai",
     packages=find_packages(exclude=("test", "dataset", "docker", "notebooks")),
     python_requires=">=3.6",
     install_requires=get_install_requirements(),
@@ -41,5 +41,5 @@ setup(
         "Operating System :: OS Independent",
         "License :: OSI Approved :: Apache Software License",
     ],
-    entry_points={"console_scripts": ["eyenet = eyenet.cli:cli"]},
+    entry_points={"console_scripts": ["medicai = src.medicai.cli:cli"]},
 )
