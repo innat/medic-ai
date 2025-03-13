3aecb431f2a516cf0cee55556aa6f293b047d414
innat <innat.dev@gmail.com>
2025-03-13 16:23:01 +0600

    update swin unetr model

--- src/medicai/models/__init__.py
+++ src/medicai/models/__init__.py
@@ -1,2 +1,3 @@
 from .duelattention import DuelAttentionNet2D
 from .unet import UNet2D
+from .swin_unetr import SwinUNETR
--- src/medicai/models/swin_unetr.py
+++ src/medicai/models/swin_unetr.py
@@ -1133,39 +1133,58 @@ def UnetrHead(
 
 
 
-def vswin_tiny(input_shape=(8, 224, 224, 1)):
-    model = VideoSwinBackboneV2(
-        input_shape=input_shape,
-        patch_size=[2, 2, 2],
-        depths=[2, 2, 2, 2],
-        window_size=[7, 7, 7],
-        num_heads=[3, 6, 12, 24],
-        embed_dim=48,
-        attn_drop_rate=0.0,
-        drop_path_rate=0.0,
-        patch_norm=False
-    )
-    return model
+class SwinUNETR(keras.Model):
+    def __init__(
+        self,
+        *,
+        input_shape=(96,96,96,1),
+        out_channels=4, 
+        feature_size=48, 
+        res_block=True, 
+        norm_name="instance",
+        **kwargs
+    ):
+        encoder = VideoSwinBackbone(
+            input_shape=input_shape,
+            patch_size=[2, 2, 2],
+            depths=[2, 2, 2, 2],
+            window_size=[7, 7, 7],
+            num_heads=[3, 6, 12, 24],
+            embed_dim=48,
+            attn_drop_rate=0.0,
+            drop_path_rate=0.0,
+            patch_norm=False
+        )
+        inputs = encoder.input
+        skips = [
+            encoder.get_layer("patching_and_embedding").output,
+            encoder.get_layer("swin_feature1").output,
+            encoder.get_layer("swin_feature2").output,
+            encoder.get_layer("swin_feature3").output,
+            encoder.get_layer("swin_feature4").output,
+        ]
+        unetr_head = UnetrHead(
+            out_channels=out_channels,
+            feature_size=feature_size, 
+            res_block=True, 
+            norm_name=norm_name, 
+        )
+        
+        # Combine encoder and decoder
+        outputs = unetr_head([inputs] + skips)
+        super().__init__(inputs=inputs, outputs=outputs, **kwargs)
 
+        self.out_channels = out_channels
+        self.feature_size = feature_size
+        self.res_block = res_block
+        self.norm_name = norm_name
 
-encoder = vswin_tiny(input_shape=(96, 96, 96, 1))
-inputs = encoder.input
-
-# Get the outputs of the Swin Transformer stages
-skips = [
-    encoder.get_layer("patching_and_embedding").output,  # hidden_states_out[0]
-    encoder.get_layer("swin_feature1").output,  # hidden_states_out[1]
-    encoder.get_layer("swin_feature2").output,  # hidden_states_out[2]
-    encoder.get_layer("swin_feature3").output,  # hidden_states_out[3]
-    encoder.get_layer("swin_feature4").output,  # hidden_states_out[4]
-]
-unetr_head = UnetrHead(
-    out_channels=4,
-    feature_size=48, 
-    res_block=True, 
-    norm_name="instance", 
-)
-
-# Combine encoder and decoder
-outputs = unetr_head([inputs] + skips)
-model_keras = Model(inputs, outputs)
+    def get_config(self):
+        config = {
+            "input_shape": self.input_shape[1:],
+            "out_channels": self.out_channels,
+            "feature_size": self.feature_size,
+            "res_block": self.res_block,
+            "norm_name": self.norm_name
+        }
+        return config
