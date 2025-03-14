f8c43e3d19c10addf271507d473422ade46bdbfb
innat <innat.dev@gmail.com>
2025-03-14 19:15:24 +0600

    add relative path

--- src/medicai/__init__.py
+++ src/medicai/__init__.py
@@ -5,11 +5,10 @@ import warnings
 def warn(*args, **kwargs):
     pass
 
-
 warnings.warn = warn
 warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
 
 from medicai import datasets, nets, utils
 
---
+++ src/medicai/blocks/__init__.py
@@ -0,0 +1,5 @@
+from medicai.blocks.unet_basic import UnetBasicBlock
+from medicai.blocks.unet_out import UnetOutBlock
+from medicai.blocks.unet_residual import UnetResBlock
+from medicai.blocks.unetr_basic import UnetrBasicBlock
+from medicai.blocks.unetr_up import UnetrUpBlock
