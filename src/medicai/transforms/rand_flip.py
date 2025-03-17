bb084b07b9a6a51ce422760ce1898efd4407b5f0
innat <innat.dev@gmail.com>
2025-03-17 12:38:15 +0600

    update random flip

--- src/medicai/transforms/rand_flip.py
+++ src/medicai/transforms/rand_flip.py
@@ -28,36 +28,7 @@ class RandFlip:
         label = inputs['label']
 
         if tf.random.uniform([]) < self.prob:
-            image = self._rand_flip(image)
-            label = self._rand_flip(label)
+            image = tf.reverse(image, axis=spatial_axis)
+            label = tf.reverse(label, axis=spatial_axis)
 
         return {'image': image, 'label': label}
-
-    def _rand_flip(self, image: tf.Tensor) -> tf.Tensor:
-        """
-        Randomly flip a 3D image tensor along specified axes.
-
-        Args:
-            image (tf.Tensor): Input 3D image tensor with shape (d, h, w, 1).
-
-        Returns:
-            tf.Tensor: Flipped image tensor.
-        """
-        axes = self._get_flip_axes()
-        if axes:
-            image = tf.reverse(image, axis=axes)
-        return image
-
-    def _get_flip_axes(self) -> list[int]:
-        """
-        Get the axes along which to flip the image.
-
-        Returns:
-            list[int]: List of axes to flip.
-        """
-        if self.spatial_axis is None:
-            return [0, 1, 2]  # Flip along all spatial axes (depth, height, width)
-        elif isinstance(self.spatial_axis, int):
-            return [self.spatial_axis]  # Flip along a single axis
-        else:
-            return list(self.spatial_axis)  # Flip along multiple axes
