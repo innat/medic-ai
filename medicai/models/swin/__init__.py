5d46c3e22038e5f4833307dd024261b8ec72ab24
innat <innat.dev@gmail.com>
2025-08-30 15:39:15 +0600

    fix rel import

--- medicai/models/swin/__init__.py
+++ medicai/models/swin/__init__.py
@@ -1,9 +1,2 @@
-from .swin_layers import (
-    SwinBasicLayer,
-    SwinPatchingAndEmbedding,
-    SwinPatchMerging,
-    SwinTransformerBlock,
-    SwinWindowAttention,
-)
 from .swin_transformer import SwinTransformer
 from .swin_unetr import SwinUNETR
