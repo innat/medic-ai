1f3f4e9f7c71689419aa2c74df54c032777272e2
innat <innat.dev@gmail.com>
2025-11-04 21:28:08 +0600

    lazy import

--- medicai/__init__.py
+++ medicai/__init__.py
@@ -1,5 +1,6 @@
-from medicai import dataloader, models, utils
+import importlib
 
+__version__ = "0.0.3"
 __all__ = [
     "models",
     "transforms",
@@ -10,4 +11,20 @@ __all__ = [
     "utils",
 ]
 
-__version__ = "0.0.3"
+
+def __getattr__(name):
+    """
+    Lazily import submodules when accessed as attributes.
+    Example:
+        import medicai
+        medicai.losses  -> loads medicai.losses only when needed
+    """
+    if name in __all__:
+        module = importlib.import_module(f"medicai.{name}")
+        globals()[name] = module
+        return module
+    raise AttributeError(f"module 'medicai' has no attribute '{name}'")
+
+
+def version():
+    return __version__
