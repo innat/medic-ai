5a2be81da558b4ac618291ad3d208816add428bd
innat <innat.dev@gmail.com>
2026-08-08 14:08:59 +0600

    Add random choice train step test

--- test/transforms/test_tf_graph_mode.py
+++ test/transforms/test_tf_graph_mode.py
@@ -77,6 +77,52 @@ def test_dual_mode_transforms_can_run_inside_custom_train_step():
 
 
 @pytest.mark.unit
+def test_random_choice_can_run_inside_custom_train_step():
+    class ChoiceAugmentedModel(keras.Model):
+        def __init__(self):
+            super().__init__()
+            self.transforms = RandomChoice(
+                transforms=[
+                    ShiftIntensity(keys=["image"], offset=0.25, input_mode="batch"),
+                    Flip(keys=["image"], spatial_axis=1, input_mode="batch"),
+                ],
+                num_choices=1,
+                prob=1.0,
+                seed=11,
+            )
+            self.backbone = keras.Sequential(
+                [
+                    keras.layers.Input(shape=(8, 8, 1)),
+                    keras.layers.Conv2D(4, 3, padding="same", activation="relu"),
+                    keras.layers.GlobalAveragePooling2D(),
+                    keras.layers.Dense(1),
+                ]
+            )
+
+        def call(self, inputs, training=False):
+            return self.backbone(inputs, training=training)
+
+        def train_step(self, data):
+            x, y = data
+            x = self.transforms({"image": x})["image"]
+            return super().train_step((x, y))
+
+    model = ChoiceAugmentedModel()
+    model.compile(
+        optimizer=keras.optimizers.Adam(1e-3),
+        loss=keras.losses.MeanSquaredError(),
+        jit_compile=False,
+    )
+
+    x = as_tensor(np.random.randn(4, 8, 8, 1).astype(np.float32))
+    y = as_tensor(np.random.randn(4, 1).astype(np.float32))
+
+    logs = model.train_on_batch(x, y, return_dict=True)
+
+    assert "loss" in logs
+
+
+@pytest.mark.unit
 def test_intensity_transforms_run_under_tf_function():
     normalize = NormalizeIntensity(keys=["image"], nonzero=True)
     scale = ScaleIntensityRange(
